b03a138eca3a4195f7163a12cf57ea3ea5f88a3c
Alexis Saint-Jean <asaintjean@gmail.com>
2018-12-06 17:30:41 -0500

    Update service-worker.js

--- webapp/service-worker.js
+++ webapp/service-worker.js
@@ -1,5 +1,6 @@
 self.addEventListener('install', (event) => {
-	console.log('service worker installed', event);
+	//console.log('service worker installed', event);
+	console.log('updated service worker installed', event);
 });
 self.addEventListener('activate', (event) => {
 	console.log('service worker activated', event);
