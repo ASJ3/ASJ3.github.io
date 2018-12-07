716237f679851201333201ada7cc96ab352cc8d9
Alexis Saint-Jean <asaintjean@gmail.com>
2018-12-07 11:19:50 -0500

    Update service-worker.js
    Adding cache

--- webapp/service-worker.js
+++ webapp/service-worker.js
@@ -1,7 +1,26 @@
+//self.addEventListener('install', (event) => {
+	//console.log('service worker installed', event);	
+//});
+
 self.addEventListener('install', (event) => {
-	//console.log('service worker installed', event);
-	console.log('updated service worker installed', event);
+  console.log('updated service worker installed', event);
+	
+  if (!('caches' in self)) return;
+  event.waitUntil(
+    caches.open('version1').then((cache) => {
+      return cache.addAll(
+        [
+          '/graph01.html',
+	  '01.png'
+          //'/styles/pirates.css',
+          //'/styles/pirates.tff',
+          //'/images/i-love-pirates.jpg'
+        ]
+      );
+    })
+  );
 });
+
 self.addEventListener('activate', (event) => {
 	console.log('service worker activated', event);
 });
