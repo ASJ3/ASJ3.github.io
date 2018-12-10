9ca5e03e31cbace448b8bc0734ea6391b2b9b9a2
Alexis Saint-Jean <asaintjean@gmail.com>
2018-12-10 09:41:13 -0500

    Add files via upload
    Adding Files from pw from the last 12-07-2018 save. I think in that version, the Add to Home Screen was working properly (for some reason pw is not working properly after the changes I made on 12-08-2018)

---
+++ cw/service-worker.js
@@ -0,0 +1,36 @@
+self.addEventListener('install', (event) => {
+  console.log('updated service worker installed', event);
+	
+  if (!('caches' in self)) {
+	  console.log('browser does not deal with cache');	  
+	  return;
+  }
+  event.waitUntil(
+    caches.open('version1').then((cache) => {
+      console.log('browser accepts cache');
+      return cache.addAll(
+        [
+          'index.html',
+		'optimization1.html',
+		'optimization2.html',
+		'recall.html',
+		'report.html',
+	        'script.js',
+		'styles.css',
+	        'markologosimple.png',
+		'markoReport2.png'
+        ]
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+  );
+});
+
+self.addEventListener('activate', (event) => {
+	console.log('service worker activated', event);
+});
