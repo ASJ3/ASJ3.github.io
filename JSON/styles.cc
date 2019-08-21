f4ff27afd2f6aa325b657f6e0ae692fb1070d2f9
Alexis Saint-Jean <asaintjean@gmail.com>
2019-08-21 14:09:18 -0400

    Create styles.cc

---
+++ JSON/styles.cc
@@ -0,0 +1,29 @@
+body {
+	background-color:#464646;
+	font-family: Arial,sans-serif; 
+	font-size:11pt;
+	margin:62px 0 0 0;
+}
+
+.oldcard {
+	max-width: 500px;
+	margin:15px auto 18px auto;
+	background-color: #fafafa;
+	border:1px solid #eaeaea;
+	/*border:1px solid #888888;*/
+	border-radius:14px;
+	overflow: hidden;
+	padding:0 0 0px 0;
+	box-shadow: 2px 2px 3px #1c1c1c;
+}
+
+.card {
+	width: 490px;
+	margin: auto;
+	background-color: white;
+	padding: 55px 65px;
+	border: solid 1px #424344;
+	border-radius: 5px;
+	margin-bottom:40px;
+	box-shadow: 4px 4px 12px #444;
+}
