147c5e7652182fdf0ec6877976470c27d55c357d
Alexis Saint-Jean <asaintjean@gmail.com>
2019-09-04 20:52:54 -0400

    created app.js and service worker sw.js 	new file:   JSON/js/app.js 	new file:   JSON/sw.js

---
+++ JSON/sw.js
@@ -0,0 +1,11 @@
+// install service worker
+self.addEventListener('install', (evt) => {
+	// this message appears in the console only once: when a new service worker is first installed
+	// if there are no changes to the service worker, then no message appears in the console
+	console.log('service worker has been installed');
+});
+
+// activate service worker
+self.addEventListener('activate', (evt) => {
+	console.log('service worker has been activated');
+});
