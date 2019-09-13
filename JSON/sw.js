0c28dbc450ddad04f89c5044a639966a0600b71b
Alexis Saint-Jean <asaintjean@gmail.com>
2019-09-13 14:45:55 -0400

    cache assets in sw.js
    caching assets when the service worker sw.js is initially being installed

--- JSON/sw.js
+++ JSON/sw.js
@@ -1,8 +1,25 @@
+const staticCacheName = 'site-static';
+const assets = [
+	'/',
+	'/index.html',
+	'/details.html',
+	'/styles3.css',
+	'/js/app.js',
+	'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'
+];
+
 // install service worker
 self.addEventListener('install', (evt) => {
 	// this message appears in the console only once: when a new service worker is first installed
 	// if there are no changes to the service worker, then no message appears in the console
-	console.log('service worker has been installed');
+	//console.log('service worker has been installed');
+	evt.waitUntil(
+		caches.open(staticCacheName).then(cache => {
+			console.log('caching shell assets');
+			cache.addAll(assets);
+		})
+	);
+	
 });
 
 // activate service worker
